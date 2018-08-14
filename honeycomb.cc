916a38ee06ddd65e24465f110b33692e4bf9e603
ipsita84 <ipsita.mandal@gmail.com>
2018-08-14 19:32:59 -0400

    up

--- honeycomb.cc
+++ honeycomb.cc
@@ -1,6 +1,6 @@
 //g++ -std=c++11 -Wall -O3 honeycomb.cc -o testo
 // vim: set ai et cin ts=4 sw=4 tw=80:
-//drawing the honeycomb lattice
+//drawing Kitaev honeycomb lattice & calculating energy + magnetization
 
 #include <iostream>
 #include <iomanip>
@@ -21,10 +21,8 @@ boost::random::mt19937 gen;
 using namespace std;
 
 const double pi = acos(-1.0);
-//const double pi = 3.14;
 
 // Define lattice constants
-double J = 1.0;
 const int axis1 = 10, axis2 = axis1;
 const int no_of_sites = 2*axis1*axis2;
 
@@ -49,8 +47,6 @@ int main(int argc, char const * argv[])
 
     array_2d_float sitepos(boost::extents[no_of_sites][2]);
     array_2d_float sitespin(boost::extents[3][no_of_sites]); 
-    array_2d_int A(boost::extents[axis1][axis2]);//A sublattice has even numbered sites
-    array_2d_int B(boost::extents[axis1][axis2]);//B sublattice has even odd sites 
 
     std::array <double, 2> h = {0,0};
   
@@ -74,31 +70,19 @@ int main(int argc, char const * argv[])
 	}
 	fpout.close();
 
-    int counter = 0;
-	for (unsigned int j = 0; j < axis2 ; ++j)
+	for (unsigned int j = 0; j < no_of_sites ; ++j)
 	{	
-        for (unsigned int i = 0; i < axis1 ; ++i)        
-        {   counter = counter +1 ;
-            A[i][j] = 2*counter;
-            B[i][j] = A[i][j]-1;
-            //printf (" % d",A[i][j]);
-            //printf (" % d",B[i][j]);
 
             double theta = roll_coin(0,pi);
             double phi = roll_coin(0,2*pi);
-            sitespin[0][A[i][j]-1] = sin(theta)*cos(phi);
-            sitespin[1][A[i][j]-1] = sin(theta)*sin(phi);
-            sitespin[2][A[i][j]-1] = cos(theta); 
-
-            theta = roll_coin(0,pi);
-            phi = roll_coin(0,2*pi);
-            sitespin[0][B[i][j]-1] = sin(theta)*cos(phi);
-            sitespin[1][B[i][j]-1] = sin(theta)*sin(phi);
-            sitespin[2][B[i][j]-1] = cos(theta); 
+            sitespin[0][j] = sin(theta)*cos(phi);
+            sitespin[1][j] = sin(theta)*sin(phi);
+            sitespin[2][j] = cos(theta); 
+            double checksum= pow(sitespin[0][j],2) + pow(sitespin[1][j],2)
+                      + pow(sitespin[2][j],2);
+            if (checksum > 1.000001) {printf (" initial %f error \n", checksum);}
 
         }
-        //printf ("\n");
-	}
 
     array_2d_float J(boost::extents[3][3]);
     double mx=0, my=0, mz=0;
@@ -109,15 +93,67 @@ int main(int argc, char const * argv[])
     ofstream fout("Energy1_hx.dat", std::fstream::app);
 
 
-
+    double energy(0);
+    double en_sum;
 	
 	return 0;
 }
 
 
 
+//function to calculate total energy
+//for a given spin configuration
+//with open boundary conditions
+
+double energy_tot(array_2d_float sitespin, array_2d_float J, std::array <double, 2> h)
+{
+    double energy = 0;
+
+    for (unsigned int i = 0; i < no_of_sites ; ++i)
+        {
+              energy += -h[0]*sitespin[0][i]-h[1]*sitespin[1][i];
+            
+        }
+
+    array_2d_int A(boost::extents[axis1][axis2]);//A sublattice has even numbered sites
+    array_2d_int B(boost::extents[axis1][axis2]);//B sublattice has even odd sites 
 
 
+    int counter = 0;
+	for (unsigned int j = 0; j < axis2 ; ++j)
+	{	
+        for (unsigned int i = 0; i < axis1 ; ++i)        
+        {   counter = counter +1 ;
+            A[i][j] = 2*counter;
+            B[i][j] = A[i][j]-1;
+            //printf (" % d",A[i][j]);
+            //printf (" % d",B[i][j]);
+        }
+        //printf ("\n");
+    }
+
+    for (unsigned comp1  = 0; comp1 < 3; ++comp1)
+    {
+        for (unsigned comp2  = comp1; comp2 < 3; ++comp2)
+        {
+
+            for (unsigned int i = 0; i < axis1 ; ++i)
+            {
+                for (unsigned int j = 0; j < axis2 ; ++j)
+                {
+                  energy += J[comp1][comp2]*sitespin[comp1][A[i][j]]
+                                           *sitespin[comp2][B[i][j]];
+                  energy += J[comp2][comp1]*sitespin[comp2][A[i][j]]
+                                           *sitespin[comp1][B[i][j]];
+                }
+            }
+        }
+    }
+
+
+
+    return energy;
+}
 
 
 
