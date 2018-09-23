a77d45ecbabc78f04ee11016ebd53d0ddde36bf5
ipsita84 <ipsita.mandal@gmail.com>
2018-09-23 16:24:37 -0400

    up

--- test.cc
+++ test.cc
@@ -1,4 +1,4 @@
-//g++ -std=c++11 -Wall -O3 honeycomb_cool-once_spins_recorded.cc -o testo
+//g++ -std=c++11 -Wall -O3 test.cc -o testo
 // vim: set ai et cin ts=4 sw=4 tw=80:
 //drawing Kitaev honeycomb lattice & calculating energy + magnetization
 // in-plane vectors are r1=(0,1,-1), r2= =(-1,1,0), r3 =(0,1,-1) as they lie on 
@@ -41,26 +41,6 @@ typedef boost::multi_array < double, 2 > array_2d_float;
 typedef boost::multi_array < int, 2 > array_2d_int;
 
 
-//Function templates
-int roll_coin(int a, int b);
-double random_real(int a, int b);
-
-double energy_tot(array_2d_float sitespin, array_2d_float J1,array_2d_float J2,
-       array_2d_float J3,std::array <double, 3> h, array_2d_int A, 
-       array_2d_int B, array_2d_int rotateleftA, array_2d_int cornerA);
-
-double nn_energy(array_2d_float sitespin, array_2d_float J1, 
-                  array_2d_float J2,array_2d_float J3,
-                  std::array <double, 3> h, array_2d_int A, array_2d_int B, 
-array_2d_int rotateleftA, array_2d_int cornerA,array_2d_int rotaterightB, 
-array_2d_int cornerB, int row, int col, int sublat);
-
-
-//No.of Monte Carlo updates we want
-const unsigned int nmore=1;
-const unsigned int N_mc = 1e5;
-const unsigned int heating = N_mc;
-
 
 const double k= -60.0, g = 30.0 ;
 const double hmag =50.0;
@@ -74,22 +54,10 @@ const double hmag =50.0;
 int main(int argc, char const * argv[])
 {     	
 	
-    ifstream gin("J.dat");
-    for (unsigned int comp1=0; comp1<3; ++comp1)
-    {
-        for (unsigned int comp2=0; comp2<3; ++comp2)
-        {
 
-            gin>>J1[comp1][comp2];
-            printf (" % f\n",J1[comp1][comp2]);
-
-        }
-    }
-
-ofstream fpout("pos.dat");	// Opens a file for output
 
     array_2d_float sitepos(boost::extents[no_of_sites][2]);
-    array_2d_float sitespin(boost::[no_of_sites]extents[3]); 
+    array_2d_float sitespin(boost::extents[no_of_sites][3]); 
 
     
 
@@ -102,6 +70,32 @@ ofstream fpout("pos.dat");	// Opens a file for output
 
   
     f1out << "qx\t qy \t Sq \t  Sq-squared "<<endl;
+    int lab;
+
+
+
+    ifstream gin(string("Spins_T_" +T_str+"_h_"+hmag_str + "_minusK_" + k_str 
+                 + "_G_"+g_str+".dat").c_str(),ios_base::app); 
+
+    string headers;
+    for(unsigned int i=1;i<=6; ++i)
+    {gin>>headers;}
+
+    for (unsigned int label=0; label< no_of_sites; ++label)
+    {
+            gin>>lab;
+            gin>>sitepos[lab][0];
+            gin>>sitepos[lab][1];
+            gin>>sitespin[lab][0];
+            gin>>sitespin[lab][1];
+            gin>>sitespin[lab][2];
+            printf ("%d % f %f %f %f %f\n",lab,sitepos[lab][0],sitepos[lab][1],
+                     sitespin[lab][0],sitespin[lab][1],sitespin[lab][2]);
+
+        
+        printf (" \n");
+    }
+
 
 
 /////////////////////////MAIN LOOP STARTS//////////////////////////////////////
@@ -113,47 +107,24 @@ ofstream fpout("pos.dat");	// Opens a file for output
         for (unsigned int qysteps=0; qysteps<101; ++qysteps)
         {
             
-
-                for (unsigned int l = 0; l < no_of_sites; ++l)
-                {
-                  mx += sitespin[0][l] ;
-                  mx_array[i -1] += sitespin[0][l] ;
- 
-                  my += sitespin[1][l] ;
-                  my_array[i -1] += sitespin[1][l];
-
-                  mz += sitespin[2][l] ;
-                  mz_array[i -1] += sitespin[2][l];
-                  sout << setw(3) << l+1;
-                  sout.setf( ios_base::fixed, ios_base::floatfield );
-                  sout.precision(7);sout << setw(12)<< sitepos[l][0]
-                  <<setw(12)<< sitepos[l][1]
-                  << setw(12)<< sitespin[0][l]
-                  << setw(12)<< sitespin[1][l]
-                  << setw(12)<< sitespin[2][l]<< endl;
-                }
+           double qy = 0 + qysteps*2*pi/100 ;
+           for (unsigned int l = 0; l < no_of_sites; ++l)
+           {
+                    int count=1;
+           }
              
 
        
-        fout.setf( ios_base::fixed, ios_base::floatfield );
-        fout.precision(2);fout << setw(6) << theta;
-        fout.precision(7);fout << setw(25)<< en_sum / (no_of_sites*N_mc) 
-                          << setw(15)<< sqrt(sigma_en) / (no_of_sites*N_mc)
-                          << setw(12)<< sqrt(sigma_en) / (no_of_sites*N_mc)
-                          << setw(12)<< mx / (no_of_sites*N_mc)
-                          << setw(12)<< sqrt(sigma_mx)/(no_of_sites*N_mc) 
-                          << setw(12)<< my / (no_of_sites*N_mc)
-                          << setw(12)<< sqrt(sigma_my)/(no_of_sites*N_mc)
-                          << setw(12)<< mz /(no_of_sites*N_mc)   
-                          << setw(12)<< sqrt(sigma_mz)/(no_of_sites*N_mc)
+        f1out.setf( ios_base::fixed, ios_base::floatfield );
+        f1out.precision(7);f1out<< setw(12)<< qx
+                          << setw(12)<< qy
+                          << setw(12)<<0
+                          << setw(12)<< 0
                           << endl;
-        // printing energy and mag to file
+        // printing to file
 
- 
+        }
 
-
- 
-        mx=0; my=0; mz=0;
     }
  
 
